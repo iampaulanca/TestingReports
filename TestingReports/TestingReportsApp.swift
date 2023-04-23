58a86dcab15871fb0716044dfe5821fe6b2d0b5f
g054573 <paul.x.ancajima@kp.org>
2023-04-22 17:26:20 -0700

    first commit

---
+++ TestingReports/TestingReportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingReportsApp.swift
+//  TestingReports
+//
+//  Created by Paul Ancajima on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingReportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
